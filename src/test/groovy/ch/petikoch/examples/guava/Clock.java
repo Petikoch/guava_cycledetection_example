efaa63fb78cb45b14a944802b9896a58b40ca44e
peti <petikoch@gmail.com>
2015-04-23 17:07:53 +0200

    improved Clock

--- src/test/groovy/ch/petikoch/examples/guava/Clock.java
+++ src/test/groovy/ch/petikoch/examples/guava/Clock.java
@@ -1,6 +1,7 @@
 package ch.petikoch.examples.guava;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.base.Throwables;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -18,24 +19,34 @@ public class Clock {
         internalTime = new AtomicLong(startTime);
     }
 
-    public void advanceTime(){
-        internalTime.incrementAndGet();
+    public void advanceTime() {
+        long time = internalTime.incrementAndGet();
+        System.out.println("Time is now: " + time);
     }
 
-    public void awaitTime(long time){
-        while(internalTime.get() < time){
-            Thread.yield();
+    public void awaitTime(long time) {
+        try {
+            awaitTime(time, Long.MAX_VALUE);
+        } catch (TimeoutException e) {
+            throw Throwables.propagate(e);
         }
     }
 
     public void awaitTime(long time, long timeoutMs) throws TimeoutException {
+        System.out.println("Thread '" + Thread.currentThread().getName() + "' is waiting for time: " + time);
         Stopwatch stopWatch = Stopwatch.createStarted();
-        while(internalTime.get() < time){
+        Stopwatch showLifeSignStopWatch = Stopwatch.createStarted();
+        while (internalTime.get() < time) {
             Thread.yield();
-            if(stopWatch.elapsed(TimeUnit.MILLISECONDS) >= timeoutMs){
-              throw new TimeoutException();
+            if (stopWatch.elapsed(TimeUnit.MILLISECONDS) >= timeoutMs) {
+                throw new TimeoutException();
+            }
+            if (showLifeSignStopWatch.elapsed(TimeUnit.SECONDS) >= 1) {
+                showLifeSignStopWatch = Stopwatch.createStarted();
+                System.out.println("Thread '" + Thread.currentThread().getName() + "' is still waiting for time: " + time);
             }
         }
+        System.out.println("Thread '" + Thread.currentThread().getName() + "' time " + time + " arrived.");
     }
 
     @Override
